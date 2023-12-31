af4632ab6dc5f99f9df4026508bda19bd8a5c6ae
ronnieaban <154830439+ronnieaban@users.noreply.github.com>
2024-01-01 02:07:11 +0700

    Update worker.js fix cache

--- worker.js
+++ worker.js
@@ -1,47 +1,47 @@
-const CACHE_DURATION = 5; // Cache 5 detik
+const CACHE_DURATION = 30; // Cache 30 detik
 
 addEventListener('fetch', (event) => {
   event.respondWith(handleRequest(event.request, event));
 });
 
 async function handleRequest(request, event) {
-  // Check jika response suda ter-cache
   const cache = caches.default;
-  const cachedResponse = await cache.match(request.url);
+  const cacheKey = new Request(request.url, { method: request.method, headers: request.headers });
+
+  // Check if the response is already cached
+  const cachedResponse = await cache.match(cacheKey);
   if (cachedResponse) {
     return cachedResponse;
   }
 
-  // Ganti dengan kunci API Google Sheets API Anda
+  // Replace with your Google Sheets API key
   const apiKey = 'GOOGLE_API_KEY';
 
-  // Ambil parameter dari URL query
+  // Extract parameters from the URL query
   const url = new URL(request.url);
   const spreadsheetId = url.searchParams.get('spreadsheetId');
   const sheetName = url.searchParams.get('sheetName');
 
-  // Periksa apakah spreadsheetId dan sheetName tersedia
+  // Check if spreadsheetId and sheetName are available
   if (!spreadsheetId || !sheetName) {
-    return new Response('Contoh https://api.ronnieaban.workers.dev/?spreadsheetId=1AhRiyMazpSLJBoJ7l32jyLQwZVdCkAJC6_UbL-mTxE8&sheetName=Kalimantan&filterColumn1=Home State&filterValue1=NY&filterColumn2=Major&filterValue2=Art', { status: 400 });
+    return new Response('Example: https://satu.qarniuwaisal565.workers.dev/?spreadsheetId=1AhRiyMazpSLJBoJ7l32jyLQwZVdCkAJC6_UbL-mTxE8&sheetName=Kalimantan&filterColumn1=Home State&filterValue1=NY&filterColumn2=Major&filterValue2=Art', { status: 400 });
   }
 
-  // URL untuk mengakses Google Sheets API
+  // URL to access Google Sheets API
   const apiUrl = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${sheetName}?key=${apiKey}`;
 
-  // Lakukan permintaan ke Google Sheets API
+  // Make a request to Google Sheets API
   const response = await fetch(apiUrl);
 
-  // Periksa apakah permintaan berhasil
+  // Check if the request is successful
   if (!response.ok) {
-    return new Response('Gagal mengambil data dari Google Sheets API', { status: response.status });
+    return new Response('Failed to fetch data from Google Sheets API', { status: response.status });
   }
 
-  // Ambil data dari respons
+  // Extract data from the response
   const data = await response.json();
-
-  // Ubah data menjadi format JSON standar
   const values = data.values;
-  const headers = values.shift(); // Ambil baris pertama sebagai header kolom
+  const headers = values.shift(); // Take the first row as column headers
   const jsonData = values.map((row) => {
     const obj = {};
     headers.forEach((header, index) => {
@@ -50,7 +50,7 @@ async function handleRequest(request, event) {
     return obj;
   });
 
-  // Ambil parameter filter dari URL query
+  // Extract filter parameters from the URL query
   const filters = [];
   for (const [key, value] of url.searchParams.entries()) {
     if (key.startsWith('filterColumn') && value) {
@@ -61,32 +61,37 @@ async function handleRequest(request, event) {
     }
   }
 
-  // Jika ada filter, lakukan filtering
+  // If there are filters, perform filtering
   if (filters.length > 0) {
-    // Filter data berdasarkan filter yang diberikan
+    // Filter data based on the provided filters
     const filteredData = jsonData.filter((row) =>
       filters.every((filter) => row[filter.column] === filter.value)
     );
 
-    // Buat respons JSON dari data yang telah difilter
+    // Create a JSON response from the filtered data
     const jsonHeaders = { 'Content-Type': 'application/json' };
     const filteredJsonResponse = new Response(JSON.stringify(filteredData), { headers: jsonHeaders });
 
-    // Set header Cache-Control untuk memberikan informasi caching pada sisi browser
+    // Set Cache-Control header to provide caching information on the browser side
     filteredJsonResponse.headers.set('Cache-Control', `public, max-age=${CACHE_DURATION}`);
 
-    // Kirim respons dengan data yang telah difilter
+    // Cache the filtered response
+    event.waitUntil(cache.put(cacheKey, filteredJsonResponse.clone()));
+
+    // Return the filtered response
     return filteredJsonResponse;
   }
 
-  // Jika tidak ada filter, kirim respons dengan data asli
+  // If there are no filters, return the original data
   const jsonHeaders = { 'Content-Type': 'application/json' };
   const jsonResponse = new Response(JSON.stringify(jsonData), { headers: jsonHeaders });
-  event.waitUntil(cache.put(request.url, jsonResponse.clone()));
 
-  // Atur header Cache-Control untuk memberikan informasi caching pada sisi browser
+  // Set Cache-Control header to provide caching information on the browser side
   jsonResponse.headers.set('Cache-Control', `public, max-age=${CACHE_DURATION}`);
 
-  // Kirim respons dengan data yang telah diambil
+  // Cache the original response
+  event.waitUntil(cache.put(cacheKey, jsonResponse.clone()));
+
+  // Return the original response
   return jsonResponse;
 }
